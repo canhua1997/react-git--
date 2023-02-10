5f554b972d7ed5fac4cdfb0478304f04930340b4
Canhua Zhang <1094277487@qq.com>
2023-02-10 11:30:59 +0800

    [canhua] refactor:  use fetch to refact

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
-import axios from "axios";
 import PubSub from 'pubsub-js'
 
 class Search extends Component {
-    searchIcon = ()=>{
+    searchIcon = async ()=>{
         //获取用户输入
         const {keyWordElement:{value:keyWord}} = this
         //发送请求前通知List更新状态
         PubSub.publish('change',{isFirst:false,isLoading:true})
-        //发送网络请求
+
+        //发送网络请求 axios
+        /*
         axios.get(`/api1/search/users?q=${keyWord}`)
             //请求成功后通知List更新状态
             .then((response)=> PubSub.publish('change',{isLoading:false,users:response.data.items}))
             //请求失败后通知App更新状态
             .catch(error => PubSub.publish('change',{isLoading:false,err:error.message}))
+         */
+
+        //发送网络请求使用fetch(未优化)
+        /*
+        fetch(`/api1/search/users?q=${keyWord}`)
+            .then(response => response.json())
+            .then( data => console.log(data.items))
+            .catch(error => PubSub.publish('change',{isLoading:false,err:error.message}))
+
+         */
+
+        //发送网络请求使用fetch(优化)
+        try{
+            const response = await  fetch(`/api1/search/users?q=${keyWord}`)
+            const data = await response.json()
+            PubSub.publish('change',{isLoading:false,users:data.items})
+        }catch (e){
+            PubSub.publish('change',{isLoading:false,err:e.message})
+        }
+
     }
 
     render() {
